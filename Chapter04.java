f76633f835a3cdc85e8d83f70b206a4b4bf74ded
EC2 Default User <fumipro0913@gmail.com>
2022-11-15 14:16:46 +0000

    基本文法,変数

---
+++ Chapter04.java
@@ -0,0 +1,13 @@
+public class Chapter04 {
+  public static void main (String[] args) {
+
+    String name = "takamura fumihito";
+    System.out.println(name);
+
+    name = "orange";
+    System.out.println(name);
+
+    final int number = 100;
+    System.out.println(number);
+  }
+}
---
+++ Ready02.java
@@ -0,0 +1,6 @@
+public class Ready02 {
+  public static void main(String[] args) {
+    // メソッド処理を記述
+    System.out.println("Hello World!");
+  }
+}
